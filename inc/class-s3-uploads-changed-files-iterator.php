6977ae798f6d9172e77c5d956bf9531015a2dfa9
Joe Hoyle <joehoyle@gmail.com>
2014-05-04 21:14:51 -0400

    Fixed --dry-run on new files

--- inc/class-s3-uploads-changed-files-iterator.php
+++ inc/class-s3-uploads-changed-files-iterator.php
@@ -7,17 +7,18 @@ class S3_Uploads_ChangedFilesIterator extends Aws\S3\Sync\ChangedFilesIterator {
 
 		$current = $this->current();
 
+		if ( ! empty( $this->dry_run ) && defined( 'WP_CLI' ) ) {
+			if ( $current->getMTime() > $data[1] ) {
+				WP_CLI::line( "(dry-run) Uploading {$current->getPathname()}" );
+			}
+			return false;			
+		}
+
 		$key = $this->sourceConverter->convert((string) $current);
 		if (!($data = $this->getTargetData($key))) {
 			return true;
 		}
 
-		if ( ! empty( $this->dry_run ) && defined( 'WP_CLI' ) ) {
-			if ( $current->getMTime() > $data[1] ) {
-				WP_CLI::line( "(dry-run) Uploading {$current->getPathname()} ({$data[0]} bytes)" );
-			}
-			return false;			
-		}
 		// Ensure it hasn't been modified since the mtime
 		return $current->getMTime() > $data[1];
 	}
