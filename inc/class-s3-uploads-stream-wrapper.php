cd3a129074c0ce3cf06ba64f80121eaef3901e67
Rob Gabaree <rob@rawb.net>
2015-08-22 12:05:20 -0400

    unregister the 's3' stream wrapper if it's already registered
    Prevents 'Warning: stream_wrapper_register(): Protocol s3:// is already defined.' messages from popping up when run via cli.

--- inc/class-s3-uploads-stream-wrapper.php
+++ inc/class-s3-uploads-stream-wrapper.php
@@ -8,6 +8,11 @@ class S3_Uploads_Stream_Wrapper extends Aws\S3\StreamWrapper {
 	 * @param S3Client $client Client to use with the stream wrapper
 	 */
 	public static function register( Aws\S3\S3Client $client) {
+		
+		if ( in_array( 's3', stream_get_wrappers() ) ) {
+                	stream_wrapper_unregister( 's3' );
+          	}
+          
 		stream_wrapper_register( 's3', __CLASS__, STREAM_IS_URL );
 		static::$client = $client;
 	}
