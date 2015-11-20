d2e648500d24fe5bf2960b84585818876e418697
Fränk Klein <contact@fklein.info>
2015-11-20 10:46:39 +0100

    Use correct comparison for the PHP version check.
    When using `>=`, PHP version 5.3.3 would be recognized as too old,
    while corresponding to the requirements.
    
    Using `>` fixes this, as it restricts the outdated notice to anything
    __below__ 5.3.3.

--- s3-uploads.php
+++ s3-uploads.php
@@ -41,7 +41,7 @@ function s3_uploads_init() {
  * @return bool True if the requirements are met, else false.
  */
 function s3_uploads_check_requirements() {
-	if ( version_compare( '5.3.3', PHP_VERSION, '>=' ) ) {
+	if ( version_compare( '5.3.3', PHP_VERSION, '>' ) ) {
 		if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
 			add_action( 'admin_notices', 's3_uploads_outdated_php_version_notice' );
 		}
