bf3b533aed03a998340b2e2a9abed55cb754168f
Joe Hoyle <joehoyle@gmail.com>
2020-05-18 15:15:02 +0200

    Install deps with plugin-tester

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -6,7 +6,10 @@ fi
 
 mkdir /tmp/s3-uploads-tests/tests
 
-docker run --name s3-uploads-tests-minio -d --rm -p 9000:9000 -e MINIO_ACCESS_KEY=AWSACCESSKEY -e MINIO_SECRET_KEY=AWSSECRETKEY -v /tmp/s3-uploads-tests:/data minio/minio server /data > /dev/null
+docker run --rm --name s3-uploads-tests-minio -d --rm -p 9000:9000 -e MINIO_ACCESS_KEY=AWSACCESSKEY -e MINIO_SECRET_KEY=AWSSECRETKEY -v /tmp/s3-uploads-tests:/data minio/minio server /data > /dev/null
 
-docker run -e S3_UPLOADS_BUCKET=tests -e S3_UPLOADS_KEY=AWSACCESSKEY -e S3_UPLOADS_SECRET=AWSSECRETKEY -e S3_UPLOADS_REGION=us-east-1 -v $PWD:/code humanmade/plugin-tester $@
+docker run --rm -e S3_UPLOADS_BUCKET=tests -e S3_UPLOADS_KEY=AWSACCESSKEY -e S3_UPLOADS_SECRET=AWSSECRETKEY -e S3_UPLOADS_REGION=us-east-1 -v $PWD:/code humanmade/plugin-tester $@
 docker kill s3-uploads-tests-minio > /dev/null
+
+echo "Running Psalm..."
+docker run --rm -v $PWD:/code --entrypoint='/code/vendor/bin/psalm' humanmade/plugin-tester
