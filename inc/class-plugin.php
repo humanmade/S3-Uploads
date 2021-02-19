4ff03c148127ea23d08dfe70c39973f9b0752935
goldenapples <goldenapplesdesign@gmail.com>
2021-02-19 12:10:57 -0500

    Add array shape description for psalm

--- inc/class-plugin.php
+++ inc/class-plugin.php
@@ -409,9 +409,9 @@ class Plugin {
 	 * Saving the filesize in the attachment metadata when the image is
 	 * uploaded allows core to skip this stat when retrieving and formatting it.
 	 *
-	 * @param array $metadata      Attachment metadata.
+	 * @param array{width: int, height: int, file?: string, sizes: array, image_meta: array} $metadata      Attachment metadata.
 	 * @param int   $attachment_id Attachment ID.
-	 * @return array Attachment metadata array, with "filesize" value added.
+	 * @return array{width: int, height: int, file?: string, sizes: array, image_meta: array, filesize: int} Attachment metadata array, with "filesize" value added.
 	 */
 	function set_filesize_in_attachment_meta( array $metadata, int $attachment_id  ) : array {
 		$file = get_attached_file( $attachment_id );
