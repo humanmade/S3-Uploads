069cab852955a1e09a57bc65d31a48cada650d6b
goldenapples <goldenapplesdesign@gmail.com>
2021-02-19 12:38:14 -0500

    Mark filesize property as optional.

--- inc/class-plugin.php
+++ inc/class-plugin.php
@@ -411,7 +411,7 @@ class Plugin {
 	 *
 	 * @param array{file?: string} $metadata      Attachment metadata.
 	 * @param int                  $attachment_id Attachment ID.
-	 * @return array{file?: string, filesize: int} Attachment metadata array, with "filesize" value added.
+	 * @return array{file?: string, filesize?: int} Attachment metadata array, with "filesize" value added.
 	 */
 	function set_filesize_in_attachment_meta( array $metadata, int $attachment_id ) : array {
 		$file = get_attached_file( $attachment_id );
