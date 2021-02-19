ce82bc4e1667f9dd06237c2343683237955bc170
goldenapples <goldenapplesdesign@gmail.com>
2021-02-19 11:51:16 -0500

    Get attached file with get_attached_file
    The attachment metadata array doesn't always have a "file" attribute.
    This uses the core API for getting the attached file rather than
    building it from the file path and the upload dir.

--- inc/class-plugin.php
+++ inc/class-plugin.php
@@ -110,7 +110,7 @@ class Plugin {
 		add_filter( 'wp_resource_hints', [ $this, 'wp_filter_resource_hints' ], 10, 2 );
 
 		add_action( 'wp_handle_sideload_prefilter', [ $this, 'filter_sideload_move_temp_file_to_s3' ] );
-		add_filter( 'wp_generate_attachment_metadata', [ $this, 'set_filesize_in_attachment_meta' ] );
+		add_filter( 'wp_generate_attachment_metadata', [ $this, 'set_filesize_in_attachment_meta' ], 10, 2 );
 
 		add_action( 'wp_get_attachment_url', [ $this, 'add_s3_signed_params_to_attachment_url' ], 10, 2 );
 		add_action( 'wp_get_attachment_image_src', [ $this, 'add_s3_signed_params_to_attachment_image_src' ], 10, 2 );
@@ -409,13 +409,12 @@ class Plugin {
 	 * Saving the filesize in the attachment metadata when the image is
 	 * uploaded allows core to skip this stat when retrieving and formatting it.
 	 *
-	 * @param array $metadata Attachment metadata.
+	 * @param array $metadata      Attachment metadata.
+	 * @param int   $attachment_id Attachment ID.
 	 * @return array Attachment metadata array, with "filesize" value added.
 	 */
-	function set_filesize_in_attachment_meta( array $metadata ) : array {
-		$uploads_dir = wp_upload_dir();
-
-		$file = path_join( $uploads_dir['basedir'], $metadata['file'] );
+	function set_filesize_in_attachment_meta( array $metadata, int $attachment_id  ) : array {
+		$file = get_attached_file( $attachment_id );
 
 		if ( file_exists( $file ) ) {
 			$metadata['filesize'] = filesize( $file );
