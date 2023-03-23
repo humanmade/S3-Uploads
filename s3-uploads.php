191ad95591d6e3ef857552c32bd1c3f25c990f39
Darren Pinder <darren@vatu.dev>
2023-03-23 10:30:08 +0000

    Fix version number
    The version number in the main plugin file is out of date with the release.

--- s3-uploads.php
+++ s3-uploads.php
@@ -8,11 +8,6 @@ Version: 3.0.4
 Author URI: https://hmn.md
 */
 
-if ( ! class_exists( '\\Aws\\S3\\S3Client' ) ) {
-        // Require AWS Autoloader file.
-       require_once dirname( __FILE__ ) . '/vendor/autoload.php';
-}
-
 require_once __DIR__ . '/inc/namespace.php';
 
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
